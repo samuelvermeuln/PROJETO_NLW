bce9c71e2575d0b5081ffb94525942ea25cd3930
samuelvermeuln <70477826+samuelvermeuln@users.noreply.github.com>
2020-12-21 21:19:21 -0300

    estado funcionando

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,15 +1,25 @@
-function popularteUFs () {
-    const ufselect = document.querySelector("select[same=uf]")
+    // .catch      tratamento de erro caso o then de errado - revisar depois 
+    //  pode ser assim ou conforme está abaixo                 .then ( (res) => {return res.jason})    
+
+function populateUFs() {
+    const ufSelect = document.querySelector("select[name=uf]")
 
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    // .catch      tratamento de erro caso o then de errado - revisar depois 
-    //  pode ser assim ou conforme está abaixo                 .then ( (res) => {return res.jason})
-    .then ( res    =>   res.jason (  )   )
-    .then ( )
+    .then( res => res.json() )
+    .then( states => {
+
+        for ( const state of states ) {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+        
+    })
 }
 
-// document
-//         .querySelector("select[name=uf]")
-//         .addEventListener("change",     ()  =>  {
-//             console.log("mudei")
-//         }    )
+populateUFs()
+
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", ( ) => {
+        console.log("mudei")
+    })
