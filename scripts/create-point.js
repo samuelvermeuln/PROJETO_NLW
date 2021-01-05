2d5f8309ca228d9b880f84008a9e33a0fa1f29ed
samuelvermeuln <70477826+samuelvermeuln@users.noreply.github.com>
2021-01-05 20:23:38 -0300

    INICIO DO ITENS DE COLETA

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -34,9 +34,8 @@ function getCities(event) {
     //     citySelect.removeChild(citySelect.firstChild)        
     //   }
     // essa parte abaixo faz a mesma coisa e porém deixa "Selecione a Cidade" aparecendo 
-    citySelect.innerHTML  = `<option value="">Selecione a Cidade</option>`
-
-
+    citySelect.innerHTML  = `<option value> Selecione a Cidade </option>`
+    citySelect.disabled = true
     //      target -> ele ta pegando o valor de "select[name=uf]"
     //          o value e o state.id
     //console.log(event.target.value)
@@ -45,16 +44,62 @@ function getCities(event) {
         .then(res => res.json())
         .then(cities => {
             for (const city of cities) {
-                citySelect.innerHTML  += `<option value="${city.id}">${city.nome}</option>`                
+                citySelect.innerHTML  += `<option value="${city.nome}">${city.nome}</option>`                
                 }                
                 citySelect.disabled = false
             })
 } 
 
-
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities)
-    
-        
     // .addEventListener("change", getCities) não passo os ( ) -> se eu passar ela executa na hora e preciso esperar antes de execultar quando mudar ele vai execultar
+
+//                                           INICIO DO ITENS DE COLETA 
+
+//pegar todos os li´s
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for ( const item of itemsToCollect) {
+    item.addEventListener("click", handleselectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems= []
+
+function handleselectedItem(event) {
+    const itemLi = event.target
+
+    // Adicionar ou remover uma classe com javaScript
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    // Verifica se existe itens selecionados, se sim 
+    // pega os itens selecionados 
+
+    const alReadySelected  = selectedItems.findIndex ( item => {
+        const itemFound = item == itemId // isso será true ou false
+        return itemFound
+    } )
+
+// se ja estiver selecionado 
+if (alReadySelected >= 0){
+    // Tira da seleção
+    const filteredItems = selectedItems.filter( item => {
+        const itemIsDifferent = item != itemId // tem que Retorna falta
+        return itemIsDifferent
+    })
+    selectedItems = filteredItems
+}else {
+    // se não tiver selecionado 
+    // adiciona á seleção
+    selectedItems.push(itemId)
+}
+
+// atualiza o campo escondido com os itens selecionados (input)
+collectedItems.value = selectedItems
+
+
+}
